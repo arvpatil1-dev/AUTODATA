2ac420f1bc2b8aa7f08a781d3f4f59c3a1e4c212
Arvind Patil <arvpatil1@gmail.com>
2026-07-31 23:21:07 +0000

    ourcharges added

--- models/ORDERS.sql
+++ models/ORDERS.sql
@@ -6,7 +6,7 @@ select
     ORDER_DATE,
     QUANTITY,
     QUANTITY*12 AS FINALPRICE,
-    quantity*3 as  discount
-    
+    quantity*3 as  discount,
+    quantity*33 as ourcharges
 from {{ source('raw', 'ORDERS') }}
 
